52a3764e94aa5cf692f625c1a9d7a4fb73cde2a5
Kevin Jimenez <kevinricardojs@gmail.com>
2015-10-19 18:55:19 -0600

    a ubuntu

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -2,5 +2,5 @@ class Producto < ActiveRecord::Base
 	belongs_to :usuario
 	belongs_to :departamento
 	has_attached_file :imagen, styles: { mini: '48x48', pequena:'200x200' ,media: '851x315' }
-	validates_attachment_content_type :imagen, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	validates_attachment_content_type :imagen, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
